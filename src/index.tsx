1aee3409d031ed797c53eb8c0f42dcffb5949e5e
sungyeounju <yj0606yj@naver.com>
2024-03-24 23:12:12 +0900

    update : style, theme

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
+import { darkTheme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -9,7 +11,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <RecoilRoot>
-      <App />
+      <ThemeProvider theme={darkTheme}>
+        <App />
+      </ThemeProvider>
     </RecoilRoot>
   </React.StrictMode>
 );
---
+++ src/style.d.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from 'styled-components';
+declare module 'styled-components'{
+    export interface DefaultTheme{
+        bgColor:string;
+        boardColor:string;
+        cardColor:string;
+    }
+}
